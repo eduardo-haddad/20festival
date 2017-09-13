c0fb8c48c19310d836b1e3f62712be306d9584c3
Unknown <eduardo.torquemada@gmail.com>
2017-09-13 15:54:36 -0300

    logos aplicados

--- resources/views/elements/logos.blade.php
+++ resources/views/elements/logos.blade.php
@@ -4,17 +4,23 @@
 
 		<div class="institucional">
 			<header>{!! switchLang('Apoio institucional', 'Institutional support') !!}</header>
-			<img src="{!! asset('img/logos/electrica.png') !!}">
+			<img src="{!! asset('img/logos/blocos/final/electrica.png') !!}">
 		</div>
 
 		<div class="cultural">
 			<header>{!! switchLang('Apoio cultural', 'Cultural support') !!}</header>
-			<img src="{!! asset('img/logos/blocos/apoio_cultural_2.png') !!}">
+			<img src="{!! asset('img/logos/blocos/final/cultural.png') !!}">
 		</div>
 
 		<div class="residencia">
 			<header>{!! switchLang('Parceiros de residência', 'Residency program partners') !!}</header>
-			<img src="{!! asset('img/logos/blocos/residencia.png') !!}">
+			<img src="{!! asset('img/logos/blocos/final/residencias.png') !!}">
+		
+		</div>
+
+		<div class="colaboracao">
+			<header>{!! switchLang('Colaboração', 'Collaboration') !!}</header>
+			<img src="{!! asset('img/logos/blocos/final/colaboracao.png') !!}">
 		
 		</div>
 	</div>
