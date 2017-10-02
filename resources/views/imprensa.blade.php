ce0a9fa35c21cd1d484a756a792296e25fa4590c
Unknown <eduardo.torquemada@gmail.com>
2017-10-02 16:33:27 -0300

    video release

--- resources/views/imprensa.blade.php
+++ resources/views/imprensa.blade.php
@@ -3,8 +3,8 @@
 
 @section('title')
 {!! switchLang(
-    'Apresentação - 20º Festival de Arte Contemporânea Sesc_Videobrasil',
-    'Introduction - 20th Contemporary Art Festival Sesc_Videobrasil') !!}
+    'Imprensa - 20º Festival de Arte Contemporânea Sesc_Videobrasil',
+    'Press - 20th Contemporary Art Festival Sesc_Videobrasil') !!}
 @stop
 
 
@@ -27,9 +27,15 @@
                 <div class="texto">
 
                     {{-- {!! switchLang('', '') !!} --}}
+
                     <p><span class="bold">Press release</span>: <a class="underline" href="/download/release_20Festival_pt.pdf">PT</a> / <a class="underline" href="/download/release_20Festival_en.pdf">EN</a></p>
 
-                    <p><span class="bold">{!! switchLang('Imagens (obras)', 'Images (works)') !!}</span>: <a class="underline" href="http://videobrasil.org.br/press/20festival/imagens_imprensa/20FestivalSescVideobrasil_imagens.zip">Download</a> / <a class="underline" href="https://www.flickr.com/photos/videobrasil/sets/72157686595495121"  target="_blank">Flickr</a></p>
+                    <p><span class="bold">Video release</span>: <a class="underline" href="https://vimeo.com/233507757" target="_blank">{!! switchLang('Assistir', 'Watch') !!}</a></p>
+
+                    <p><span class="bold">{!! switchLang('Seleção de imagens (obras)', 'Image selection (works)') !!}</span>: <a class="underline" href="http://videobrasil.org.br/press/20festival/imagens_imprensa/20FestivalSescVideobrasil_imagens.zip">Download</a> / <a class="underline" href="https://www.flickr.com/photos/videobrasil/sets/72157686595495121"  target="_blank">Flickr</a></p>
+
+
+                    
                     
                 </div>
             </div>
