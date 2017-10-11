77aef38041f0148fea92483921c4b0f04e7fd632
Unknown <eduardo.torquemada@gmail.com>
2017-10-11 18:28:40 -0300

    imprensa

--- resources/views/imprensa.blade.php
+++ resources/views/imprensa.blade.php
@@ -34,6 +34,27 @@
 
                     <p><span class="bold">{!! switchLang('Seleção de imagens (obras)', 'Image selection (works)') !!}</span>: <a class="underline" href="http://videobrasil.org.br/press/20festival/imagens_imprensa/20FestivalSescVideobrasil_imagens.zip">Download</a> / <a class="underline" href="https://www.flickr.com/photos/videobrasil/sets/72157686595495121"  target="_blank">Flickr</a></p>
 
+                    
+                        <p><br><br><span class="bold">A4&Holofote</span><br>
+                        Cristiane Nascimento<br>
+                        <a href="mailto:cristianenascimento@a4eholofote.com.br">cristianenascimento@a4eholofote.com.br</a><br>
+                        Neila Carvalho<br>
+                        <a href="mailto:neilacarvalho@a4eholofote.com.br">neilacarvalho@a4eholofote.com.br</a><br>
+                        +55 11 3897 4122</p>
+                         
+                        <p><span class="bold">{!! switchLang('Assessoria de Imprensa', 'Press relations') !!} | Sesc Pompeia</span><br>
+                        Roberta Della Noce e Fernanda Porta Nova<br>
+                        Camila Cetrone {!! switchLang('(estagiária)', '(intern)') !!}<br>
+                        <a href="mailto:imprensa@pompeia.sescsp.org.br">imprensa@pompeia.sescsp.org.br</a><br>
+                        +55 11 3871 7720</p>
+                         
+                        <p><span class="bold">{!! switchLang('Comunicação', 'Communication') !!} | Associação Cultural Videobrasil</span><br>
+                        Marcele Rocha e Cecília do Val<br>
+                        <a href="mailto:comunicacao@videobrasil.org.br">comunicacao@videobrasil.org.br</a><br>
+                        +55 11 3645 0516</p>
+
+                    
+
 
                     
                     
